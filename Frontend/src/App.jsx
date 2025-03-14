1e80229206c986857a9e9f7f6e2b52b96b86f166
Udaymore741 <orionlee741@gmail.com>
2025-03-14 10:41:35 +0530

    Modified App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,13 @@ import { Toaster } from "./components/ui/toaster";
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { TooltipProvider } from "./components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Questions from "./pages/Questions";
 import QuestionDetail from "./pages/QuestionDetail";
 import Profile from "./pages/Profile";
 import Auth from "./pages/Auth";
+import Colleges from "./pages/Colleges";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,17 +18,18 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <Router>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/questions" element={<Questions />} />
-          <Route path="/question/:id" element={<QuestionDetail />} />
-          <Route path="/profile/:id" element={<Profile />} />
           <Route path="/login" element={<Auth />} />
           <Route path="/signup" element={<Auth />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/question/:id" element={<QuestionDetail />} />
+          <Route path="/colleges" element={<Colleges />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </Router>
     </TooltipProvider>
   </QueryClientProvider>
 );
